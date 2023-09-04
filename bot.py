ea58a847802df6d4b0885c78ef55cbc367298b88
Rᴀᴅʜᴀ Sɪɴɢʜ <133753917+Radhak8@users.noreply.github.com>
2023-09-04 13:33:15 +0530

    aww 🥺

--- bot.py
+++ bot.py
@@ -252,7 +252,7 @@ if __name__ == "__main__":
         raise Exception("Your API_ID/API_HASH is not valid.")
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
-    print(f"""JOIN  @RadhaX2Support
+    print(f"""JOIN  @RadhaSupport
 GIVE STAR TO THE REPO 
  {BOT_NAME} ɪs ᴀʟɪᴠᴇ!  
     """)
